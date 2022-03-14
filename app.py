1b7e983ad6eb2a99655099d2db3f4487c6326ecf
Andrey Kim <engineering.volosati@gmail.com>
2022-03-14 16:06:37 +0800

    too much shit

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
+from turtle import delay
 from flask import Flask, send_from_directory, Response
-import os
+import socket
+import os, time
 from datetime import datetime
-import time
 
+serv = 0
+addr = ("", 3000)  # all interfaces, port 8080
 app = Flask(__name__)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def send_my_message(msg, __method = 1):
+    global serv
+    msg = bytes(msg.encode('utf-8'))
+    print("method called")
+    if serv != 1:
+        if __method == None:
+            s.bind(addr)
+        else:
+            s = socket.create_server(addr)
+        serv = 1
+
+    s.sendall(msg)
+    s.send(msg)
+    data = s.recv(1024)
+    print(data)
+    s.close()
+
 
 react_folder = 'streaming-tutorial'
 directory= os.getcwd()+ f'/{react_folder}/build/static'
@@ -22,6 +44,7 @@ def css(folder,file):
     ''' User will call with with thier id to store the symbol as registered'''
     
     path = folder+'/'+file
+        
     return send_from_directory(directory=directory,path=path)
 
 @app.route('/stream')
@@ -32,9 +55,14 @@ def stream():
         while True:
             #gotcha
             time.sleep(1)
+            
+            msg = "Hello World TEST"
+            # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            #     s.bind(('0.0.0.0', 3050))
+            send_my_message(msg)
             yield f'data: {datetime.now().second} \n\n'
 
     return Response(get_data(), mimetype='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    resp = app.run(debug=True)
